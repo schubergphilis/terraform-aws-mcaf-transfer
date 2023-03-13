309325bee042027c44fe68df18b596faaefff3eb
Enri Peters <epeters@schubergphilis.com>
2023-03-13 17:03:00 +0100

    Add support for on_partial_upload

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 locals {
-  user_policy  = data.aws_iam_policy_document.user_policy.json
-  vpc_endpoint = var.vpc_endpoint != null ? { create = true } : {}
-  on_upload    = var.on_upload != null ? { create = true } : {}
+  user_policy       = data.aws_iam_policy_document.user_policy.json
+  vpc_endpoint      = var.vpc_endpoint != null ? { create = true } : {}
+  on_upload         = var.on_upload != null ? { create = true } : {}
+  on_partial_upload = var.on_partial_upload != null ? { create = true } : {}
 
   user_ssh_keys = { for item in flatten([
     for user, config in var.users : [
@@ -68,13 +69,26 @@ resource "aws_transfer_server" "default" {
     }
   }
 
+  # this Terraform code block creates one or both of the on_upload and on_partial_upload blocks,
+  # depending on whether the corresponding input variables are defined and can be accessed.
   dynamic "workflow_details" {
-    for_each = local.on_upload
+    for_each = can(local.on_upload) || can(local.on_partial_upload) ? [1] : []
 
     content {
-      on_upload {
-        execution_role = var.on_upload.execution_role
-        workflow_id    = var.on_upload.workflow_id
+      dynamic "on_upload" {
+        for_each = can(local.on_upload) ? [1] : []
+        content {
+          execution_role = var.on_upload.execution_role
+          workflow_id    = var.on_upload.workflow_id
+        }
+      }
+
+      dynamic "on_partial_upload" {
+        for_each = can(local.on_partial_upload) ? [1] : []
+        content {
+          execution_role = var.on_partial_upload.execution_role
+          workflow_id    = var.on_partial_upload.workflow_id
+        }
       }
     }
   }
