1a5ca8a1e658d00c5dcacb9ede3b797fd450a901
Thomas Moreau <thomas.moreau@devolksbank.nl>
2025-12-15 12:30:27 +0100

    test: add tests

---
+++ tests/setup/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
+
+resource "random_string" "default" {
+  length  = 4
+  special = false
+  upper   = false
+}
+
+output "random_string" {
+  value = random_string.default.id
+}
